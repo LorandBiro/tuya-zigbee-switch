0fe6541b9c90534c401b3f7ee093e6efec7054e2
Loránd Biró <lorand.biro@outlook.com>
2026-01-03 14:31:37 +0000

    Let's see if a custom cluster registration function solves anything...

--- src/telink/hal/zigbee_zcl.c
+++ src/telink/hal/zigbee_zcl.c
@@ -5,6 +5,7 @@
 #include "zcl_multistate_input.h"
 #include "zcl_onoff_configuration.h"
 #include "zcl_cover_switch_config.h"
+#include "zcl_window_covering_custom.h"
 #pragma pack(pop)
 
 #include "telink_size_t_hack.h"
@@ -49,7 +50,8 @@ static cluster_registerFunc_t get_register_func_by_cluster_id(u16 cluster_id) {
     return zcl_multistate_input_register;
   }
   if (cluster_id == ZCL_CLUSTER_CLOSURES_WINDOW_COVERING) { // Window Covering
-    return zcl_windowCovering_register;
+    // Use custom registration to support all commands including GO_TO_LIFT_PERCENTAGE
+    return zcl_windowCovering_custom_register;
   }
   if (cluster_id == 0xFC01) { // Cover Switch Config
     return zcl_cover_switch_config_register;
